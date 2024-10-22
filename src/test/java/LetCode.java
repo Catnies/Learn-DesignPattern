03cd42e87913de06791fac980bcfe43fe89852c9
Catnies <catnies@foxmail.com>
2024-10-22 16:22:36 +0800

    Commit 1

---
+++ src/test/java/LetCode.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class LetCode {
+
+    @Test
+    public void run() {
+        System.out.println("num is " + addDigits(256));
+    }
+
+    public int[] addDigits(int num) {
+        return new int[]{2, 4};
+    }
+
+}
